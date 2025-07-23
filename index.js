9455b1743ae908084db3b522c1d946eac06dea2f
Emily Harbeck <eharbeck@me.com>
2025-07-23 14:28:14 +1000

    completed and passed all tests

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ function whisper(string) {
     return string.toLowerCase();
 }
 function logShout(string) {
-    console.log(shout(string));
+    console.log(string.toUpperCase());
 }
 function logWhisper(string) {
-    console.log(whisper(string));
+    console.log(string.toLowerCase());
 }
 function sayHiToHeadphonedRoommate(string) {
-   if (string === string.toLowerCase()){
-    return "I can't hear you!"
-   }
-    if (string === string.toUpperCase()){
-      return "YES INDEED!" 
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!"
     }
-     if (string === "Let's have dinner together!"){
-        return "I would love to!"
-     }
+     if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+    }
+       if (string === "Let's have dinner together!"){
+          return "I would love to!"
+       }
 }
-logShout ("Hello");
